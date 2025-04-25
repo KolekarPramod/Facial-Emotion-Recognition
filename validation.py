e530050f56eb9934cc4be7025d8f3d926b34530a
KolekarPramod <pkolekar940@gmail.com>
2025-04-25 15:06:31 +0000

    added new model

--- validation.py
+++ validation.py
@@ -1,12 +1,7 @@
 from ultralytics import YOLO
 
-# Load a model
-model = YOLO("yolo11s.pt")  # load an official model
+
 model = YOLO("yolo11s_emotion.pt")  # load a custom model
 
 # Validate the model
-metrics = model.val(data ="./dataset/images")  
-metrics.box.map  # map50-95
-metrics.box.map50  # map50
-metrics.box.map75  # map75
-metrics.box.maps  # a list contains map50-95 of each category
+metrics = model.val(data ="./dataset/images/train_split")  
---
+++ yolo_streaming.py
@@ -0,0 +1,7 @@
+from ultralytics import solutions
+
+inf = solutions.Inference(
+    model="yolo11n.pt",  # you can use any model that Ultralytics support, i.e. YOLO11, YOLOv10
+)
+
+inf.inference()
